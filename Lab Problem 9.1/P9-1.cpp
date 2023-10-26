14e95507541502243ad5d7a604424252e986e9de
Wesley Casselman <w-casselman@onu.edu>
2023-10-26 08:26:27 -0400

    Can now display every element in order of appearance in the array

--- Lab Problem 9.1/P9-1.cpp
+++ Lab Problem 9.1/P9-1.cpp
@@ -1,3 +1,42 @@
 // P9-1
 // Wesley Casselman
 // October 26th of 2023
+
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+void printArray(int array[], int size);
+
+int main(void)
+{
+	srand(time(0));
+	
+	const int SIZE = 10;
+	
+	int arr[SIZE];
+	for (int i = 0; i < SIZE; i++)
+	{
+		do
+		{
+			arr[i] = rand() % 10000;
+		} while (arr[i] < 1000);
+
+		
+	}
+
+	printArray(arr, SIZE);
+
+	return 0;
+}
+
+void printArray(int array[], int size)
+{
+	cout << "The random integers: ";
+	for (int i = 0; i < size; i++)
+	{
+		cout << array[i] << "  ";
+	}
+	return;
+}
